0c04fb36d07a8b1594f2967727de3eb912f32281
Christian <christianpefer@gmail.com>
2025-11-06 18:34:54 +0100

    init project

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { serve, setup } from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Body Parser
+app.use(express.json());
+
+// Swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "My Express.js API",
+      version: "1.0.0",
+      description: "A sample Express.js API built with TypeScript and Swagger",
+    },
+  },
+  apis: ["./src/routes/*.ts"],
+};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use("/docs", serve, setup(swaggerDocs));
+
+app.listen(port, () =>
+  console.log(`Listening on port ${port} - http://localhost:${port}`)
+);
